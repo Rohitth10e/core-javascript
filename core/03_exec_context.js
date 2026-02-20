7d81cd56f1026a30e95e1b62d3c4f5fbfecd5609
rohith10e <rohith.e.s82@gmail.com>
2026-02-20 14:42:44 +0530

    feat: js-exec flow

---
+++ core/03_exec_context.js
@@ -0,0 +1,11 @@
+let val1 = 10
+let val2 = 5
+
+function addNum(a, b){
+    return a + b
+}
+
+let result1 = addNum(val1, val2)
+let result2 = addNum(10, 20)
+
+// for call stack concept refer call_stack.png
